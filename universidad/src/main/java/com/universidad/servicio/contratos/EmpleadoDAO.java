c4069b9998585a1c7925cdadbc26089eaf5e1aa4
Jared Pineda <mateo12molina@gmail.com>
2022-09-19 17:33:09 -0600

    eliminando

--- universidad/src/main/java/com/universidad/servicio/contratos/EmpleadoDAO.java
+++
@@ -1,8 +0,0 @@
-package com.universidad.servicio.contratos;
-
-import com.universidad.modelo.entidades.Persona;
-import com.universidad.modelo.entidades.enumeradores.TipoEmplado;
-
-public interface EmpleadoDAO extends PersonaDAO{
-    Iterable<Persona> findEmpleadoByTipoEmpleado(TipoEmplado tipoEmplado);
-}
